40f0c02140cc2dd239d3b64af12f5fbd217994cf
Gibson <mwangigibson71@gmail.com>
2026-08-18 14:37:14 +0300

    Load universal CMS content on every page

--- client/js/config.js
+++ client/js/config.js
@@ -1,1 +1,8 @@
 const API_URL = "http://localhost:3000/api";
+window.API_URL = API_URL;
+
+if (!document.querySelector('script[src="js/content.js"]')) {
+    const contentScript = document.createElement("script");
+    contentScript.src = "js/content.js";
+    document.head.appendChild(contentScript);
+}
